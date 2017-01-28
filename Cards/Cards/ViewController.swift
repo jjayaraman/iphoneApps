8bcecd80cab0adb66406dc5c9ab34ef0ba5745da
Jayakumar Jayaraman <india.jai@gmail.com>
2017-01-28 08:22:43 +0000

    App completed and working

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var leftCardOutlet: UIImageView!
+    @IBOutlet weak var leftScoreOutlet: UILabel!
+    var leftScore = 0
+
+    @IBOutlet weak var rightCardOutlet: UIImageView!
+    @IBOutlet weak var rightScoreOutlet: UILabel!
+    var rightScore = 0
+    
+
+    var cards = ["card2", "card3", "card4", "card5", "card6","card7","card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +33,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func dealTapped(sender: UIButton) {
+        print("deal tapped")
+        
+        let leftCardIndex = Int (arc4random_uniform(12))
+        leftCardOutlet.image = UIImage(named: cards[leftCardIndex])
+        
+        let rightCardIndex = Int(arc4random_uniform(12));
+        rightCardOutlet.image = UIImage(named: cards[rightCardIndex])
+    
+        if(leftCardIndex > rightCardIndex) {
+            leftScore += 1
+        }else if leftCardIndex == rightCardIndex {
+            
+        } else {
+          //  rightScore += 1
+        }
+        
+        leftScoreOutlet.text = String(leftScore)
+        rightScoreOutlet.text = String (rightScore)
+    }
 }
 
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var cards = ["card2", "card3"]
+
+
+var leftCard = arc4random_uniform(13)
+leftCard
+
+cards[1]
