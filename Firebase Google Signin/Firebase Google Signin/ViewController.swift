4e81fb735666cfa808847eae1dd00f324f04a8bd
Jayakumar Jayaraman <india.jai@gmail.com>
2017-01-28 20:32:27 +0000

    iOS App Google sign using Firebase

---
+++ Firebase Google Signin/Firebase Google Signin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase Google Signin
+//
+//  Created by Jayakumar Jayaraman on 28/01/2017.
+//  Copyright © 2017 jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
