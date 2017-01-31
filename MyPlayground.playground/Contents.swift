279234feda260496799b7d69d002afb9fb9ab1b9
Jayakumar Jayaraman <india.jai@gmail.com>
2017-01-31 22:27:34 +0000

    swift basics

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,12 +2,38 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Data types
+var age:Int = 23
+var name:String = "Jay"
+var weight:Float = 73.4
+var salary:Double = 123000.99
+var smoke:Bool = false
 
+// Infered Data types
+var age2 = 40
+var name2 = "Kumar"
+var weight2 = 34.50
+var salary2 = 12345999.55
+var drink = true
+
+// Array
 var cards = ["card2", "card3"]
+var names = [String]()
+names = ["Simran", "Ramba", "Kusbhu"]
 
+var ages = [Int]()
+ages = [1,2,3,5,6,7,4]
+ages.append(10)
+print(ages)
 
-var leftCard = arc4random_uniform(13)
-leftCard
+// If / else
+if age == 23 {
+    print("true age is 23")
+} else {
+    print("false")
+}
 
-cards[1]
+//for loop
+for  index in 1...5 {
+    print(index)
+}
