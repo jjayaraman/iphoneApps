574ea7abaece390ea1d51587d1054ef687fb6311
Jayakumar Jayaraman <india.jai@gmail.com>
2017-02-05 18:15:02 +0000

    Firebase email authenticaton added

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
     var uName = ""
     var pass = ""
     
@@ -23,28 +24,77 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     @IBAction func loginTapped(_ sender: UIButton) {
         
         uName = userName.text!
         pass = password.text!
         
-        if uName == "admin" && pass == "admin" {
-            status.text = "Success"
-            status.backgroundColor = UIColor.green;
-        }
-        else {
-            status.text = "Invalid credentials."
-            status.backgroundColor = UIColor.red;
-        }
+        self.login()
     }
     
     
+    
+    @IBAction func registerUser(_ sender: UIButton) {
+        uName = userName.text!
+        pass = password.text!
+        
+        self.createUser()
+        
+        
+    }
+    
+    
+    // Login validation using Firebase
+    func login() {
+        
+        FIRAuth.auth()?.signIn(withEmail: uName, password: pass, completion: {
+            user, error in
+            
+            if error != nil {
+                
+                self.status.text = "Invalid credentials"
+                self.status.backgroundColor = UIColor.red
+                
+                print("Invalid credentials supplied. Error \(error)")
+                
+            }
+            else {
+                
+                self.status.text = "`logged in"
+                self.status.backgroundColor = UIColor.green
+                print("Logged succefully..")
+            }
+            
+        })
+        
+    }
+    
+    // Create new user account
+    func createUser() {
+        FIRAuth.auth()?.createUser(withEmail: uName, password: pass, completion:  {
+            user , error in
+            
+            if error != nil {
+                print("Error creeating new user")
+                self.status.text = "Try again"
+                self.status.backgroundColor = UIColor.red
+                
+            }
+            else {
+                print("New user created")
+                
+                self.status.text = "New user registered"
+                self.status.backgroundColor = UIColor.green
+            }
+        })
+        
+    }
 }
 
