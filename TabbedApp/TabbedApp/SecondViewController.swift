87d34be222e780a71745646ef737c7c8cfd94475
Jayakumar Jayaraman <india.jai@gmail.com>
2017-02-06 22:37:31 +0000

    Navigation control added

---
+++ TabbedApp/TabbedApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabbedApp
+//
+//  Created by Jayakumar Jayaraman on 06/02/2017.
+//  Copyright © 2017 jay. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
